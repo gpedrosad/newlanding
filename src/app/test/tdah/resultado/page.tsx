494ae7c95b0a8afb3f2e90090a96ec6d4321ea1d
gpedrosad <gpedrosa@uade.edu.ar>
2025-09-19 12:44:30 -0300

    cambio en unused var

--- src/app/test/tdah/resultado/page.tsx
+++ src/app/test/tdah/resultado/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useMemo, useState } from "react";
+import React, { useMemo } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { motion } from "framer-motion";
 
